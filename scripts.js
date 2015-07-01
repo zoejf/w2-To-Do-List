b31ecfa3d74db34c765ca064054631cc521e0252
zoejf <zoejfoss@gmail.com>
2015-06-30 18:21:00 -0700

    refactored with Underscore

--- scripts.js
+++ scripts.js
@@ -2,47 +2,70 @@ $(function() {
 
 	var $newItemForm = $("#new_task_item");
 	var $toDoList = $("#to_do_list");
-	var listArray = [];
-	var liArray = [{item_title: "Walk the dog", description: "for 30 mins", due_date: "2015-06-30"}, {item_title: "Do Laundry", description: "all of it", due_date: "2015-07-20"}, 
-	{item_title: "Make Lunch", description:"4 servings for the week", due_date: "2015-08-10"}];
 
-//pre-loads first three list items
-$(liArray).each (function displayItems() {
-		var $newLI = $('<li class="item"> Item: ' + this.item_title + ";     Description: " + this.description + ";     Due Date: " + this.due_date + '</li>'); 
-		$('#to_do_list').append($newLI);
-});
+	//to do template -- this is a function
+	var toDoTemplate = _.template($('#todo-template').html());
 
-	//listens for click on the "add to list" button
+	var $toDoUl = $("#todo-list");
+
+	var tasks = [
+	  {title: "Sprinkles", description: "cat", date: "2015-07-01"},
+	  {title: "Bagel", description: "dog", date: "2015-07-02"},
+	  {title: "Fluffy", description: "dinosaur", date: "2015-07-03"}
+	];
+
+	_.each(tasks, function (task, index) {
+	  var $task = $(toDoTemplate(task));
+	  $task.attr('data-index', index);
+	  $toDoUl.append($task);
+	});
+
+
+//listens for click on the "add to list" button
 	$newItemForm.on("submit", function(event) {
 		event.preventDefault();
+		
+		// if ( ($('#item_name').text() !== $('#item_name').val()) || ($('#item_desc').text() !== $('#item_desc').val()) || ($('due_date').text() !== $('#due_date').val()) ) {
+		// 	$(".alert.alert-danger").removeClass("display");
+		// } else { 
+
 		console.log('form submitted');
 		console.log($('#item_name').val() );  //shopping list name input
 		console.log($('#item_desc').val() ); // shopping list description input
 		console.log($('#due_date').val() ); // shopping list due date
 
 		//creates a new variable to store the newly submitted item
-		var $newLI = $('<li class="item"> Item: ' + $('#item_name').val() + ";    Description: " + $('#item_desc').val() + ";    Due Date: " +  $('#due_date').val() +'</li>'); 
-		
-		//adds the submitted item to the list below
-		$('#to_do_list').append($newLI);
-		
+		var newTasks = [];
+
+		//adds the submitted item to an array of new items
+		newTasks.push({title: $('#item_name').val(), description: $('#item_desc').val(), date: $('#due_date').val() });
+
+		//print the new tasks to the html
+		_.each(newTasks, function (task, index) {
+		  var $task = $(toDoTemplate(task));
+		  $task.attr('data-index', index);
+		  $toDoUl.append($task);
+		});
+
+
 		// this allows strikeout (or "done" class) for the new items
-		$listItems = $("#to_do_list .item");
+		$listItems = $("#todo-list .to_do");
 		$listItems.click(function (event) {
 			event.preventDefault();
 			$(this).addClass("done");
 			})
 
-		//push the newly submitted items to the array of items
-		listArray.push({item_title: $("#item_name").val(), description: $('#item_desc').val() });
 
-		console.log("item name: " + $("#item_name").val() + "description: " + $("#item_desc").val() );
-		console.log(listArray);
+		console.log("item title: " + $("#item_name").val() + " description: " + $("#item_desc").val() + " date: " + $('#due_date').val() );
+		console.log(tasks);
+		
+		// });
 	});
 
 
+
 // this allows strikeout for the hardcoded items
-	var $listItems = $("#to_do_list .item");
+	var $listItems = $("#todo-list .to_do");
 
 		$listItems.click(function (event) {
 			event.preventDefault();
